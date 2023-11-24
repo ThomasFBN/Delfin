4dfa19d9dc8f3a23b8f48b2b7a2a884d134fa844
OliverLyksted <144331612+OliverLyksted@users.noreply.github.com>
2023-11-24 20:09:39 +0100

    konstruktør og get-metoder til SwimTime

--- src/main/java/domain/SwimTime.java
+++ src/main/java/domain/SwimTime.java
@@ -1,5 +1,40 @@
 package domain;
 
-public class SwimTime {
+import org.w3c.dom.events.Event;
 
+import java.time.LocalDate;
+
+public class SwimTime {
+    private double time;
+    private String member;
+    private LocalDate date;
+    private Event event;
+    private String placement;
+
+    public SwimTime(double time, String member, LocalDate date, Event event, String placement){
+        this.time = time;
+        this.member = member;
+        this.date = date;
+        this.event = event;
+        this.placement = placement;
+    }
+
+    public double getTime() {
+        return time;
+    }
+    public String getMember(){
+        return member;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getPlacement() {
+        return placement;
+    }
 }
